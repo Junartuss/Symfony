69c643dfbe67efadbc70e81e140197b78585c584
Junartuss <paul-allix@outlook.fr>
2019-05-02 19:49:36 +0200

    Revert "feat: filtrage"
    This reverts commit 3fbb1b1a60967e8a4b8a20303f5103a176d8cb20.

--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\Property;
-use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
@@ -25,25 +24,10 @@ class PropertyRepository extends ServiceEntityRepository
     /**
      * @return Query
      */
-    public function findAllVisibleQuery(PropertySearch $search) : Query
+    public function findAllVisibleQuery() : Query
     {
-        $query = $this->findVisibleQuery();
-
-        if($search->getMaxPrice()){
-            $query = $query
-                ->andWhere('p.price <= :maxprice')
-                ->setParameter('maxprice', $search->getMaxPrice())
-            ;
-        }
-
-        if($search->getMinSurface()){
-            $query = $query
-                ->andWhere('p.surface >= :minsurface')
-                ->setParameter('minsurface', $search->getMinSurface())
-            ;
-        }
-
-        return $query->getQuery();
+        return $this->findVisibleQuery()
+            ->getQuery();
     }
 
     /**
